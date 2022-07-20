2b523de9ecbc0916334b15529f8fcf359f3180e5
Ilya Burchalovskiy <burchalovskiy@gmail.com>
2022-07-20 12:03:19 +0300

    AAP-12500 Устранены замечания из МР

--- internal/validator/fields/date/get_date.go
+++ internal/validator/fields/date/get_date.go
@@ -7,6 +7,13 @@ import (
 	"validation_service/pkg/log"
 )
 
+type (
+	expectedDate struct {
+		Value time.Time
+		Sub   int
+	}
+)
+
 func getPlainDate(rawValue json.RawMessage) (time.Time, bool) {
 	var value DateDateValue
 	err := json.Unmarshal(rawValue, &value)
@@ -34,7 +41,7 @@ func getDepending(rawValue json.RawMessage, selfMap, fieldsMap *sync.Map) (time.
 	var value DependingValue
 	err := json.Unmarshal(rawValue, &value)
 	if err != nil {
-		log.Logger.Error("Ошибка парсинга JSON")
+		log.Logger.Errorf("Ошибка парсинга JSON: %s", rawValue)
 		return time.Time{}, false
 	}
 
@@ -56,7 +63,7 @@ func getDependingFormula(rawValue json.RawMessage, selfMap, fieldsMap *sync.Map)
 	var formula DateDependingFormulaValue
 	err := json.Unmarshal(rawValue, &formula)
 	if err != nil {
-		log.Logger.Error("Ошибка парсинга JSON")
+		log.Logger.Errorf("Ошибка парсинга JSON: %s", rawValue)
 		return time.Time{}, false
 	}
 
@@ -69,72 +76,81 @@ func getDependingFormula(rawValue json.RawMessage, selfMap, fieldsMap *sync.Map)
 	return expectedDate, true
 }
 
-func getDependingConditionFormula(rawValue json.RawMessage, selfMap, fieldsMap *sync.Map) (time.Time, bool) {
-	type (
-		expectedDate struct {
-			Value time.Time
-			Sub   int
-		}
-	)
+func getRangeExpectedDate(formula DateDependingConditionFormulaValue, selfMap, fieldsMap *sync.Map) (time.Time, bool) {
 	var (
-		formula DateDependingConditionFormulaValue
-		result  expectedDate
+		today  time.Time
+		result expectedDate
 	)
 
-	err := json.Unmarshal(rawValue, &formula)
-	if err != nil {
-		log.Logger.Error("Ошибка парсинга JSON")
-		return time.Time{}, false
-	}
+	today = time.Now()
+	result.Value = today
 
-	switch formula.Condition.Type {
-	case "range":
-		var today = time.Now()
-		result.Value = today
-		for _, item := range formula.Condition.Items {
-			dv := DateDependingFormulaValue{
-				formula.Dependency,
-				formula.Operation,
-				item,
-				formula.Unit}
-			date, err := dv.getExpectedDate(selfMap, fieldsMap)
-			if err != nil {
-				log.Logger.Errorf("Ошибка при расчёте формулы: %s", err.Error())
-				return time.Time{}, false
-			}
-
-			switch formula.Direction {
-			case "left_border":
-				if date.Before(today) {
-					var item = expectedDate{Value: date, Sub: int(today.Sub(date))}
-					if result.Sub == 0 || item.Sub < result.Sub {
-						result = item
-					}
-
-				}
-			case "right_border":
-				if date.After(today) {
-					var item = expectedDate{Value: date, Sub: int(today.Sub(date))}
-					if result.Sub == 0 || item.Sub > result.Sub {
-						result = item
-					}
-				}
-			}
-		}
-	default:
+	for _, item := range formula.Condition.Items {
 		dv := DateDependingFormulaValue{
 			formula.Dependency,
 			formula.Operation,
-			formula.Condition.Default,
+			item,
 			formula.Unit}
-
 		date, err := dv.getExpectedDate(selfMap, fieldsMap)
 		if err != nil {
 			log.Logger.Errorf("Ошибка при расчёте формулы: %s", err.Error())
 			return time.Time{}, false
 		}
-		result.Value = date
+
+		switch formula.Direction {
+		case "gte":
+			if date.Before(today) {
+				var item = expectedDate{Value: date, Sub: int(today.Sub(date))}
+				if result.Sub == 0 || item.Sub < result.Sub {
+					result = item
+				}
+			}
+		case "lte":
+			if date.After(today) {
+				var item = expectedDate{Value: date, Sub: int(today.Sub(date))}
+				if item.Sub > result.Sub {
+					result = item
+				}
+			}
+		}
 	}
-	log.Logger.Infof("%s := %s", formula.Direction, result.Value)
 	return result.Value, true
+}
+
+func getDependingConditionFormula(rawValue json.RawMessage, selfMap, fieldsMap *sync.Map) (time.Time, bool) {
+	var (
+		formula      DateDependingConditionFormulaValue
+		ExpectedDate time.Time
+		ok           bool
+	)
+
+	err := json.Unmarshal(rawValue, &formula)
+	if err != nil {
+		log.Logger.Errorf("Ошибка парсинга JSON: %s", rawValue)
+		return time.Time{}, false
+	}
+
+	switch formula.Condition.Type {
+	case "range":
+		ExpectedDate, ok = getRangeExpectedDate(formula, selfMap, fieldsMap)
+	default:
+		ExpectedDate, ok = getDefaultExpectedDate(formula, selfMap, fieldsMap)
+	}
+
+	return ExpectedDate, ok
+}
+
+func getDefaultExpectedDate(formula DateDependingConditionFormulaValue, selfMap, fieldsMap *sync.Map) (time.Time, bool) {
+	dv := DateDependingFormulaValue{
+		formula.Dependency,
+		formula.Operation,
+		formula.Condition.Default,
+		formula.Unit}
+
+	date, err := dv.getExpectedDate(selfMap, fieldsMap)
+	if err != nil {
+		log.Logger.Errorf("Ошибка при расчёте формулы: %s", err.Error())
+		return time.Time{}, false
+	}
+	return date, true
 }
